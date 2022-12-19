7742ad0fc06bc0d16548c6dcd69c63afe4f89858
Ravindu <ACER@Ravindu>
2022-12-20 02:05:56 +0530

    MongoDB : db connection and student login done

---
+++ Library/src/main/java/com/library/db/dbConnect.java
@@ -0,0 +1,34 @@
+package com.library.db;
+import com.mongodb.client.*;
+
+  public class dbConnect {
+	
+	 static MongoDatabase database = null;
+	 
+	   public static MongoDatabase getDatabase() {
+	      if (database != null) {
+	    	  return database;
+	      }
+	      else {
+		      
+	    	  String dbname = "library";
+		      return getDatabase(dbname);
+	      }
+	   }
+	   
+		   
+	   private static MongoDatabase getDatabase(String databaseName) {
+		  
+		   try {
+			   	MongoClient mongoClient = MongoClients.create();
+			   	database = mongoClient.getDatabase(databaseName);
+			   	System.out.println("New Db connection");
+		  }catch (Exception e) {
+		         e.printStackTrace();
+		  }
+		  return database;
+		  }
+		
+	   
+}
+
